23f46e602cd43077b8b5fccee44539dfe3e40e32
Janani B. <144763322+janani-06032005@users.noreply.github.com>
2024-12-07 18:29:37 +0530

    Add files via upload

---
+++ gemini_utility.py
@@ -0,0 +1,60 @@
+import os
+import json
+from PIL import Image
+import google.generativeai as genai
+
+# Working directory path
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Path of config_data file
+config_file_path = f"{working_dir}/config.json"
+config_data = json.load(open(config_file_path))
+
+# Loading the GOOGLE_API_KEY
+GOOGLE_API_KEY = config_data["GOOGLE_API_KEY"]
+
+# Configuring google.generativeai with API key
+genai.configure(api_key=GOOGLE_API_KEY)
+
+def load_gemini_pro_model():
+    gemini_pro_model = genai.GenerativeModel("gemini-pro")
+    return gemini_pro_model
+
+# Get response from Gemini-1.5-Flash model - image/text to text
+def gemini_pro_vision_response(prompt, image):
+    gemini_1_5_flash_model = genai.GenerativeModel("gemini-1.5-flash")
+    response = gemini_1_5_flash_model.generate_content([prompt, image])
+    result = response.text
+    return result
+
+# Get response from embeddings model - text to embeddings
+def embeddings_model_response(input_text):
+    embedding_model = "models/embedding-001"
+    embedding = genai.embed_content(model=embedding_model,
+                                    content=input_text,
+                                    task_type="retrieval_document")
+    embedding_list = embedding["embedding"]
+    return embedding_list
+
+# Get response from Gemini-Pro model - text to text
+def gemini_pro_response(user_prompt):
+    gemini_pro_model = genai.GenerativeModel("gemini-pro")
+    response = gemini_pro_model.generate_content(user_prompt)
+    result = response.text
+    return result
+
+
+
+# result = gemini_pro_response("What is Machine Learning")
+# print(result)
+# print("-"*50)
+#
+#
+# image = Image.open("test_image.png")
+# result = gemini_pro_vision_response("Write a short caption for this image", image)
+# print(result)
+# print("-"*50)
+#
+#
+# result = embeddings_model_response("Machine Learning is a subset of Artificial Intelligence")
+# print(result)
---
+++ main.py
@@ -0,0 +1,103 @@
+import os
+from PIL import Image
+import streamlit as st
+from streamlit_option_menu import option_menu
+from gemini_utility import (load_gemini_pro_model,
+                            gemini_pro_response,
+                            gemini_pro_vision_response,
+                            embeddings_model_response)
+
+# Set working directory
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Configure Streamlit page
+st.set_page_config(
+    page_title="GemCapChat",
+    page_icon="🧠",
+    layout="centered",
+)
+
+# Sidebar for page selection
+with st.sidebar:
+    selected = option_menu('GemCapChat',
+                           ['ChatBot',
+                            'Image Captioning',
+                            'Embed text'],
+                           menu_icon='robot', icons=['chat-dots-fill', 'image-fill', 'textarea-t', 'patch-question-fill'],
+                           default_index=0
+                           )
+
+# Function to translate roles between Gemini-Pro and Streamlit terminology
+def translate_role_for_streamlit(user_role):
+    if user_role == "model":
+        return "assistant"
+    else:
+        return user_role
+
+# ChatBot page
+if selected == 'ChatBot':
+    model = load_gemini_pro_model()
+
+    # Initialize chat session in Streamlit if not already present
+    if "chat_session" not in st.session_state:
+        st.session_state.chat_session = model.start_chat(history=[])
+
+    st.title("🤖 ChatBot")
+
+    # Display the chat history
+    for message in st.session_state.chat_session.history:
+        with st.chat_message(translate_role_for_streamlit(message.role)):
+            st.markdown(message.parts[0].text)
+
+    # Input field for user's message
+    user_prompt = st.chat_input("Ask Gemini-Pro...")
+    if user_prompt:
+        # Add user's message to chat and display it
+        st.chat_message("user").markdown(user_prompt)
+
+        # Send user's message to Gemini-Pro and get the response
+        gemini_response = st.session_state.chat_session.send_message(user_prompt)
+
+        # Display Gemini-Pro's response
+        with st.chat_message("assistant"):
+            st.markdown(gemini_response.text)
+
+
+# Image Captioning page
+if selected == "Image Captioning":
+
+    st.title("📷 Snap Narrate")
+
+    uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+
+    if uploaded_image and st.button("Generate Caption"):
+        image = Image.open(uploaded_image)
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            resized_img = image.resize((800, 500))
+            st.image(resized_img)
+
+        default_prompt = "Write a short caption for this image"
+
+        # Get the caption of the image from the gemini-1.5-flash LLM
+        caption = gemini_pro_vision_response(default_prompt, image)
+
+        with col2:
+            st.info(caption)
+
+
+# Text Embedding page
+if selected == "Embed text":
+
+    st.title("🔡 Embed Text")
+
+    user_prompt = st.text_area(label='', placeholder="Enter the text to get embeddings")
+
+    if st.button("Get Response"):
+        response = embeddings_model_response(user_prompt)
+        st.markdown(response)
+
+
+
